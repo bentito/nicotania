5263d39cef88b83b42f8b635785f16ab4f111fb2
Brett Tofel <brett@dartmouth.edu>
2015-07-29 21:24:34 -0400

    this commit is prior to changing the database format to better handle FileTags

---
+++ src/nicotania_0/pkg1/FXMLTimeSyncUtilController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nicotania_0.pkg1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author bentito
+ */
+public class FXMLTimeSyncUtilController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
